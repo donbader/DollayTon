48510271af65b9af1fa8875673d15c610aed8540
baimao8437 <baimao8437@gmail.com>
2019-11-29 01:42:40 +0800

    fix base on comment

--- app/services/trade_bot.rb
+++ app/services/trade_bot.rb
@@ -5,7 +5,9 @@
 class TradeBot
   include Singleton
 
-  attr_reader :env, :websocket_machine, :processing_machine, :websocket_url
+  SETTINGS = YAML.load_file(Rails.root.join("setting.yml"))[Rails.env]
+
+  attr_reader :env, :websocket_machine, :processing_machine
 
   def initialize
     @env = {
@@ -16,8 +18,8 @@ class TradeBot
       current_price_data: nil,
       running: false,
     }
-    yml = YAML.load_file(Rails.root.join("setting.yml"))[Rails.env]
-    @websocket_url = yml["websocket_url"]
+
+    @websocket_url = SETTINGS["websocket_url"]
   end
 
   def run
@@ -45,7 +47,7 @@ class TradeBot
     @websocket_machine = Binance::Client::WebsocketMachine.new(@websocket_url)
     @websocket_machine.run do |event|
       env[:current_price_data] = JSON event.data
-      puts env[:current_price_data]
+
       @websocket_machine.stop if Time.now >= env[:end_time]
     end
 
